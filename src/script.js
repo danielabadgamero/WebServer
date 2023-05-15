de5e70eaa1b99acc20d824476dc6730670a81b4d
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-15 17:45:17 +0200

    Make logo undraggable.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,28 @@
 let camera = {
-    x: 0,
-    y: 0,
-    x: 0
+	x: 0,
+	y: 0,
+	x: 0
 }
 
-window.addEventListener("mousemove", (e) => {
-    console.log(e.clientX + " " + e.clientY);
+let mouse = {
+	x: 0,
+	y: 0
+}
+
+let mouseDown = false;
+
+window.addEventListener("mousemove", e => {
+	if (mouseDown) {
+		camera.x += e.movementX;
+		camera.y += e.movementY;
+	}
+	console.log(camera.x + " " + camera.y);
 });
+
+window.addEventListener("mousedown", e => {
+	mouseDown = true;
+})
+
+window.addEventListener("mouseup", e => {
+	mouseDown = false;
+})
