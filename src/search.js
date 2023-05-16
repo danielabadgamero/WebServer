e60ae254ec66c4f3e1b9863926c3881effd4337a
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-16 08:34:04 +0200

    Add distance to places.

--- src/search.js
+++ src/search.js
@@ -1,11 +1,37 @@
 let form = document.getElementById("searchForm");
+let frame = document.getElementById("results");
+let coords;
+
+navigator.geolocation.getCurrentPosition((pos) => { coords = pos.coords });
+
+// https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+	var R = 6371; // Radius of the earth in km
+	var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+	var dLon = deg2rad(lon2 - lon1);
+	var a =
+		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+		Math.sin(dLon / 2) * Math.sin(dLon / 2)
+		;
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	var d = R * c; // Distance in km
+	return d;
+}
+
+function deg2rad(deg) {
+	return deg * (Math.PI / 180)
+}
 
 form.onsubmit = (e) => {
-    e.preventDefault();
-    let http = new XMLHttpRequest();
-    http.open("POST", "search.html", false);
-    http.send("s=" + form.lastChild.nodeValue);
-    console.log(http.responseText);
-    let obj = JSON.parse(http.responseText);
-    console.log(obj);
+	e.preventDefault();
+	let http = new XMLHttpRequest();
+	http.open("POST", "search.html", false);
+	http.send("s=" + form.lastChild.nodeValue);
+	let obj = JSON.parse(http.responseText);
+
+	let card = document.createElement('div');
+	obj.forEach((act) => {
+		console.log(act);
+	})
 };
